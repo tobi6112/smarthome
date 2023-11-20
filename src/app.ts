da94f963a9039f03e0179527fd4844edb7071cee
Tobi <22715034+twobiers@users.noreply.github.com>
2023-11-20 13:01:00 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const client = mqtt.connect(`mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`);
-let timeoutId: Number | null = null;
+let timeoutId: number | null = null;
 
 const turnAllTheLightsOn = () => {
     const stateOn = {
